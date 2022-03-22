deff727b715a07f1269d6e4d0f772f28170f8e1e
Mejiao <Jonathan19991@gmail.com>
2022-03-22 14:55:35 -0500

    RandomPortfoliosProviderTest

--- HelloWorldFunction/src/test/java/stock/backend/RandomPortfoliosProviderTest.java
+++ HelloWorldFunction/src/test/java/stock/backend/RandomPortfoliosProviderTest.java
@@ -1,25 +1,44 @@
 package stock.backend;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import helloworld.App;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
 
 public class RandomPortfoliosProviderTest {
 
     @Test
     public void successfulRandomPortfoliosProvider() {
-        App app = new App();
-        APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
-        String content = result.getBody();
-        assertNotNull(content);
-        assertTrue(content.contains("\"id\""));
-        assertTrue(content.contains("\"name\""));
-        assertTrue(content.contains("\"accuracy\""));
-        assertTrue(content.contains("\"totalHoldings\""));
-        assertTrue(content.contains("\"amount\""));
-        assertTrue(content.contains("\"currency\""));
+        RandomPortfoliosProvider portfoliosProvider = new RandomPortfoliosProvider();
+        List<Portfolio> portfolios = portfoliosProvider.getPortfolios();
+        assertNotNull(portfolios);
+
+        for (Portfolio portfolio : portfolios) {
+            assertFalse(portfolio.getId().isEmpty());
+            assertFalse(portfolio.getName().isEmpty());
+            assertTrue(accuracyCheck(portfolio.getAccuracy()));
+            assertTrue(amountCheck(portfolio.getTotalHoldings().getAmount()));
+            assertEquals("USD", portfolio.getTotalHoldings().getCurrency());
+        }
+
+    }
+
+    private Boolean amountCheck(double amount) {
+        if (amount > 1 && amount < 50) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean accuracyCheck(double amount) {
+        if (amount > 0 && amount < 1) {
+            return true;
+        }
+        return false;
     }
 }
