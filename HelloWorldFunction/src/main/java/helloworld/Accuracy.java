fd360fd4a997dcfe583a83e632822e6ed46c30e2
Mejiao <Jonathan19991@gmail.com>
2022-03-22 14:03:19 -0500

    accuracy revert

--- HelloWorldFunction/src/main/java/helloworld/Accuracy.java
+++ HelloWorldFunction/src/main/java/helloworld/Accuracy.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 @Builder
 public class Accuracy {
-    private double accuracy;
-    private String foo;
-    private List<String> accounts;
+     private double accuracy;
+     private String foo;
+     private List<String> accounts;
 }
