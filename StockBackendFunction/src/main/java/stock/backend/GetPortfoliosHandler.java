dac87f1e9cd149698e1cf96526e5971213b61c38
Jonathan M <85910530+Mejiao99@users.noreply.github.com>
2022-03-25 18:04:50 -0500

    Portfolio response (#11)
    * add response

--- StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
+++ StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
@@ -4,14 +4,43 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
 import java.util.Collections;
+import java.util.List;
 
 public class GetPortfoliosHandler extends AbstractRequestHandler<GetPortfolioResponse> {
 
     @Override
     protected GetPortfolioResponse getResponse(APIGatewayProxyRequestEvent input, Context context) {
+        List<Holding> holdings = Collections.singletonList(Holding.builder()
+                .quantity(1)
+                .ticket("ticketA")
+                .build());
+
+        List<Account> accounts = Collections.singletonList(Account.builder()
+                .id("C1")
+                .holdings(holdings)
+                .build());
+
+        PortfolioDefinition portfolio = PortfolioDefinition.builder()
+                .id("Xl1")
+                .name("Port1")
+                .accounts(accounts)
+                .build();
+
+        List<PortfolioDefinition> portfolios = Collections.singletonList(portfolio);
+
+        Money price = Money.builder()
+                .amount(50.0)
+                .currency("USD")
+                .build();
+
+        List<StockPrice> stockPrices = Collections.singletonList(StockPrice.builder()
+                .ticket("ticketA")
+                .price(price)
+                .build());
+
         return GetPortfolioResponse.builder()
-                .portfolios(Collections.emptyList())
-                .stockPrices(Collections.emptyList())
+                .portfolios(portfolios)
+                .stockPrices(stockPrices)
                 .build();
     }
 }
