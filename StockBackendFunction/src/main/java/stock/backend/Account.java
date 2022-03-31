524fc69df569591badf31858a193d2e8feb16731
Jonathan M <85910530+Mejiao99@users.noreply.github.com>
2022-03-31 16:34:12 -0500

    fromJson (#15)
    * GetPortfoliosHandler uses convertFromJson

--- StockBackendFunction/src/main/java/stock/backend/Account.java
+++ StockBackendFunction/src/main/java/stock/backend/Account.java
@@ -1,12 +1,16 @@
 package stock.backend;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
     private String id;
     private Map<String,Double> holdings;
--- StockBackendFunction/src/main/java/stock/backend/Money.java
+++ StockBackendFunction/src/main/java/stock/backend/Money.java
@@ -1,10 +1,14 @@
 package stock.backend;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Money {
     private double amount;
     private String currency;
