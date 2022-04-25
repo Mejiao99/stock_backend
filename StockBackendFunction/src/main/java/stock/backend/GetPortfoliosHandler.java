e1d7e45f39fff971d9c3012a4c6563cae397a62f
Mejiao <Jonathan19991@gmail.com>
2022-04-25 16:24:22 -0500

    getAccounts from PortfolioDefinition

--- StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
+++ StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
@@ -1,6 +1,5 @@
 package stock.backend;
 
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -11,6 +10,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -23,38 +24,36 @@ public class GetPortfoliosHandler extends AbstractRequestHandler<GetPortfolioRes
     protected GetPortfolioResponse getResponse(APIGatewayProxyRequestEvent input, Context context) {
         final String contents = readContents();
         GetPortfolioResponse response = convertFromJson(contents);
-        Map<String,GetTableResponse> tablePerPortfolioDefinitions = GenerateTablePerPortfolioDefinitions();
+        Map<String, GetTableResponse> tablePerPortfolioDefinitions = generateTablePerPortfolioDefinition(response);
         response.setTablePerPortfolioDefinitions(tablePerPortfolioDefinitions);
         return response;
     }
 
-    private Map<String, GetTableResponse> GenerateTablePerPortfolioDefinitions() {
-        Map<String,GetTableResponse> response = new HashMap<>();
-        response.put("port-1",GetTableResponse.builder()
-                .accounts(List.of())
-                .tickets(List.of())
-                .data(List.of(Collections.emptyList()))
-                .totals(Totals.builder()
-                        .accounts(Collections.emptyList())
-                        .tickets(Collections.emptyList())
-                        .total(Money.builder()
-                                .amount(0)
-                                .currency("")
-                                .build())
-                        .build()).build());
-        response.put("port-2",GetTableResponse.builder()
-                .accounts(List.of())
-                .tickets(List.of())
-                .data(List.of(Collections.emptyList()))
-                .totals(Totals.builder()
-                        .accounts(Collections.emptyList())
-                        .tickets(Collections.emptyList())
-                        .total(Money.builder()
-                                .amount(0)
-                                .currency("")
-                                .build())
-                        .build()).build());
+    private Map<String, GetTableResponse> generateTablePerPortfolioDefinition(GetPortfolioResponse getPortfolioResponse) {
+        Map<String, GetTableResponse> response = new HashMap<>();
+        for (PortfolioDefinition portfolioDefinition : getPortfolioResponse.getPortfolios()) {
+            response.put(portfolioDefinition.getId(), GetTableResponse.builder()
+                    .accounts(getAccounts(portfolioDefinition))
+                    .tickets(List.of())
+                    .data(List.of(Collections.emptyList()))
+                    .totals(Totals.builder()
+                            .accounts(Collections.emptyList())
+                            .tickets(Collections.emptyList())
+                            .total(Money.builder()
+                                    .amount(0)
+                                    .currency("")
+                                    .build())
+                            .build()).build());
+        }
         return response;
+    }
+
+    private List<String> getAccounts(PortfolioDefinition portfolioDefinition) {
+        List<String> accountList = new ArrayList<>();
+        for (Account account : portfolioDefinition.getAccounts()) {
+            accountList.add(account.getId());
+        }
+        return accountList;
     }
 
     private String readContents() {
@@ -63,7 +62,7 @@ public class GetPortfoliosHandler extends AbstractRequestHandler<GetPortfolioRes
         DynamoDB dynamoDB = new DynamoDB(client);
         Table table = dynamoDB.getTable("PortfolioDefinitions");
 
-        Item row = table.getItem(new KeyAttribute("userId","x@x.com"));
+        Item row = table.getItem(new KeyAttribute("userId", "x@x.com"));
 
         String contents = row.getString("contents");
         return contents;
