515c8fce504ae346bad0b80d4df1052d677da46b
Mejiao <Jonathan19991@gmail.com>
2022-04-28 13:30:57 -0500

    sum

--- StockBackendFunction/src/main/java/stock/backend/Money.java
+++ StockBackendFunction/src/main/java/stock/backend/Money.java
@@ -17,8 +17,15 @@ public class Money {
         return Money.builder().amount(amount * factor).currency(currency).build();
     }
 
-    private Money convertCurrencyToTargetCurrency(final double conversionRate, final String targetCurrency){
+    private Money convertCurrencyToTargetCurrency(final double conversionRate, final String targetCurrency) {
         return Money.builder().amount(amount * conversionRate).currency(targetCurrency).build();
     }
 
+    private Money sum(final Money money) {
+        final String moneyCurrency = money.currency;
+        if (moneyCurrency.equals(currency)) {
+            return Money.builder().amount(amount + money.amount).currency(moneyCurrency).build();
+        }
+        return money;
+    }
 }
