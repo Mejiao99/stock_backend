e2d6c261f8f654c247a3f8f3c420b35d15f398d3
Jonathan M <85910530+Mejiao99@users.noreply.github.com>
2022-05-02 13:59:25 -0500

    .merge (#22)
    * classifyMoneyPerCurrency now use map.merge

--- StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
+++ StockBackendFunction/src/main/java/stock/backend/GetPortfoliosHandler.java
@@ -43,21 +43,6 @@ public class GetPortfoliosHandler extends AbstractRequestHandler<GetPortfolioRes
         return response;
     }
 
-    public Map<String, Money> classifyMoneyPerCurrency(List<Money> moneyList) {
-        Map<String, Money> moneyPerCurrency = new HashMap<>();
-
-        for (Money money : moneyList) {
-            final Money currentValue;
-            if (moneyPerCurrency.get(money.getCurrency()) == null) {
-                currentValue = Money.builder().amount(0).currency(money.getCurrency()).build();
-            } else {
-                currentValue = moneyPerCurrency.get(money.getCurrency());
-            }
-            final Money newValue = currentValue.sum(money);
-            moneyPerCurrency.put(currentValue.getCurrency(), newValue);
-        }
-        return moneyPerCurrency;
-    }
 
     private Totals calculateTotals(PortfolioDefinition portfolioDefinition, GetPortfolioResponse getPortfolioResponse, List<String> accountTickets) {
         return Totals.builder()
@@ -79,6 +64,12 @@ public class GetPortfoliosHandler extends AbstractRequestHandler<GetPortfolioRes
                         .currency("")
                         .build())
                 .build();
+    }
+
+    public Map<String, Money> classifyMoneyPerCurrency(List<Money> moneyList) {
+        Map<String, Money> moneyPerCurrency = new HashMap<>();
+        moneyList.forEach( money ->  moneyPerCurrency.merge(money.getCurrency(), money, (oldValue, newValue) -> oldValue.sum(money)));
+        return moneyPerCurrency;
     }
 
     private List<Money> ticketsTotals(PortfolioDefinition portfolioDefinition, Map<String, Money> stockPrices, Map<String, Double> conversionRates, String targetCurrency, List<String> tickets) {
