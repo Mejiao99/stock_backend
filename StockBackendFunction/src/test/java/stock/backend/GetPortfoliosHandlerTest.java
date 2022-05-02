589e76ef31428d35b6c534c4e54c8acdee1da74f
Mejiao <Jonathan19991@gmail.com>
2022-05-02 12:37:34 -0500

    test

--- StockBackendFunction/src/test/java/stock/backend/GetPortfoliosHandlerTest.java
+++ StockBackendFunction/src/test/java/stock/backend/GetPortfoliosHandlerTest.java
@@ -4,32 +4,44 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GetPortfoliosHandlerTest {
-    private GetPortfoliosHandler handler;
     List<Money> moneyList = new ArrayList<>();
+    private GetPortfoliosHandler handler;
 
     @BeforeEach
     public void setup() {
         handler = new GetPortfoliosHandler();
 
+        moneyList.add(Money.builder().amount(8).currency("usd").build());
+        moneyList.add(Money.builder().amount(2).currency("usd").build());
         moneyList.add(Money.builder().amount(4).currency("usd").build());
-        moneyList.add(Money.builder().amount(3).currency("usd").build());
-        moneyList.add(Money.builder().amount(5).currency("cad").build());
+
+        moneyList.add(Money.builder().amount(1).currency("cad").build());
+        moneyList.add(Money.builder().amount(9).currency("cad").build());
+        moneyList.add(Money.builder().amount(7).currency("cad").build());
+        moneyList.add(Money.builder().amount(3).currency("cad").build());
+
+        moneyList.add(Money.builder().amount(5).currency("eur").build());
     }
+
     @Test
     void classifyMoneyPerCurrency() {
         Map<String, Money> moneyPerCurrency = handler.classifyMoneyPerCurrency(moneyList);
+
         assertNotNull(moneyPerCurrency);
-        assertEquals(moneyPerCurrency.size() , 2);
-        assertEquals(moneyPerCurrency.get("usd") , validateMoney());
-        assertEquals(moneyPerCurrency.get("cad") , 7);
+        assertEquals(moneyPerCurrency.size(), 3);
+
+        validateMoney(moneyPerCurrency.get("usd"), 14, "usd");
+        validateMoney(moneyPerCurrency.get("cad"), 20, "cad");
+        validateMoney(moneyPerCurrency.get("eur"), 5, "eur");
     }
+
     private void validateMoney(Money money, double amount, String currency) {
         assertEquals(amount, money.getAmount());
         assertEquals(currency, money.getCurrency());
