cd0a2d36c723ffe41fe233328ae035f0673cd9b1
Mejiao <Jonathan19991@gmail.com>
2022-05-02 12:28:36 -0500

    test

---
+++ StockBackendFunction/src/test/java/stock/backend/GetPortfoliosHandlerTest.java
@@ -0,0 +1,14 @@
+package stock.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetPortfoliosHandlerTest {
+
+    @Test
+    void classifyMoneyPerCurrency() {
+    }
+
+
+}
