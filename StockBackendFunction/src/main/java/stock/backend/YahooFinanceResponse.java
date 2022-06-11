961c4c2b811286e8b65790d3b0c971b7a9d6ac22
Mejiao <Jonathan19991@gmail.com>
2022-06-11 15:19:40 -0500

    callSparkApi

--- StockBackendFunction/src/main/java/stock/backend/YahooFinanceResponse.java
+++ StockBackendFunction/src/main/java/stock/backend/YahooFinanceResponse.java
@@ -1,11 +1,68 @@
 package stock.backend;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class YahooFinanceResponse {
+    MarketYahoo marketYahoo = new MarketYahoo();
 
-    private List<Money> calculateStockHistoricalPrice(String ticket, LocalDate startDate, LocalDate endDate){
+    public static void main(String[] args) {
+        YahooFinanceResponse response = new YahooFinanceResponse();
+    }
+
+    private String callSparkApi(String interval, String range, String symbols) {
+        try {
+            return Files.readString(Paths.get("C:\\Users\\Jonathan\\Documents\\HelloWorldFunction\\StockBackendFunction\\src\\main\\java\\stock\\backend\\apispark.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String callQuoteApi(String symbols) {
+        try {
+            return Files.readString(Paths.get("C:\\Users\\Jonathan\\Documents\\HelloWorldFunction\\StockBackendFunction\\src\\main\\java\\stock\\backend\\apiquote.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<LocalDate, Money> calculateStockHistoricalPrice(String ticket, LocalDate startDate, LocalDate endDate) {
         return null;
+    }
+
+    private long calculateInterval(LocalDate startDate, LocalDate endDate) {
+        return endDate.getDayOfMonth() - startDate.getDayOfMonth();
+    }
+
+    private List<Timestamp> datesToTimestamps(List<LocalDate> localDates) {
+        List<Timestamp> result = new ArrayList<>();
+        for (LocalDate localDate : localDates) {
+            result.add(toTimestamp(localDate));
+        }
+        return result;
+    }
+
+    private LocalDate localDateFromTimestampLong(Integer timestampLong) {
+        return ZonedDateTime.ofInstant(Instant.ofEpochSecond(timestampLong), ZoneId.of("UTC")).toLocalDate();
+    }
+
+    private Timestamp toTimestamp(LocalDate localDate) {
+        return Timestamp.valueOf(localDate.atStartOfDay());
+    }
+
+    private List<LocalDate> datesInRange(LocalDate startDate, LocalDate endDate) {
+        List<LocalDate> result = startDate.datesUntil(endDate).collect(Collectors.toList());
+        result.add(endDate);
+        return result;
     }
 }
