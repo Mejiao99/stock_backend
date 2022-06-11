62296eb150f8801148d2687ada2502d1a55e143a
Mejiao <Jonathan19991@gmail.com>
2022-06-11 11:13:57 -0500

    calculateStockHistoricalPrice

--- StockBackendFunction/src/main/java/stock/backend/YahooFinanceResponse.java
+++ StockBackendFunction/src/main/java/stock/backend/YahooFinanceResponse.java
@@ -1,4 +1,11 @@
 package stock.backend;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class YahooFinanceResponse {
+
+    private List<Money> calculateStockHistoricalPrice(String ticket, LocalDate startDate, LocalDate endDate){
+        return null;
+    }
 }
